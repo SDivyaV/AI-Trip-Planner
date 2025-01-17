8bbc3a6d20ea01207b4e0c1a7d0e45f41a5e021b
SDivyaV <divyavsawant@gmail.com>
2025-01-17 22:00:26 +0530

    Updated routing for GitHub Pages deployment and configured HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './create-trip/Home'
-import Header from './components/ui/custom/Header'
-import ViewTrip from './view-trip/[tripId]/ViewTrip'
-import MyTrip from './my-trips/MyTrip'
-
-
+import React from "react";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Home from "./create-trip/Home";
+import Header from "./components/ui/custom/Header";
+import ViewTrip from "./view-trip/[tripId]/ViewTrip";
+import MyTrip from "./my-trips/MyTrip";
 
 const App = () => {
-  return (  
+  return (
     <div>
-      <Routes>
-        <Route path='/' element={<Header />} />
-        <Route path='/trip' element={<Home />}/>
-        <Route path='/view-trip/:tripId' element={<ViewTrip />} />
-        <Route path='/mytrip' element={<MyTrip />}/>
-     </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/trip" element={<Home />} />
+          <Route path="/view-trip/:tripId" element={<ViewTrip />} />
+          <Route path="/mytrip" element={<MyTrip />} />
+        </Routes>
+      </HashRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
